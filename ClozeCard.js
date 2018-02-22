868703b8de6e801b6fc8137308b311d930792e3a
reyna <reynamillan@hotmail.com>
2018-02-22 15:33:09 -0700

    cleaning .js pages

--- ClozeCard.js
+++ ClozeCard.js
@@ -79,12 +79,5 @@ else{
 
 askQuestions();
 
- var questionA = new BasicCard("Who was the first president of the United States?", "George Washington" );
- var questionB = new BasicCard("Who was the third president of the United States?", "Thomas Jefferson");
- var questionC = new BasicCard("Who was the fifth president of the United States?", "James Monroe" );
- var questionE = new BasicCard("What was Abraham Lincoln before he became president?", "Lawyer" );
- var questionF = new BasicCard("Who was the first president who live in the White House?", "John Adams");
- var questionG = new BasicCard("Who was the first president who own an automobile?", "William Howard Taft" );
 
-module.exports = BasicCard;
 
